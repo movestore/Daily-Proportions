844a817c68942339139d3c4304a38d377b1d87ec
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2022-07-04 13:17:48 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# Daily-Proportions
+# Daily Proportions
 calculates the daily proportions of locations (and duration) of a certain property (e.g. speed >1)
+
+MoveApps
+
+Github repository: *github.com/movestore/Daily-Proportions*
+
+## Description
+Filters your data according to a selected attribute and value and returns the daily number and cumulative duration of locations fulfilling this property.
+
+## Documentation
+This App first marks all locations that fulfill a user defined property (e.g. ground speed > 10 m/s). Then it extracts the number of locations per day and track that this property is fulfilled. Sample sizes per day are added.
+
+In addition, the cumulative daily duration is calcualted, where each track segment's duration is attributed to the start locations. The last location of each day is attributed with the time until the first location of the next day, if there are such data, else NA.
+
+For each calender date average proportion and duration is calcualted, where n.pts indicates that number of tracks providing data for this date.
+
+### Input data
+moveStack in Movebank format
+
+### Output data
+moveStack in Movebank format
+
+### Artefacts
+`Daily_Proportions.csv`: table of daily sample sizes, numbers and cumulative durations of locations that fulfill the required property.
+
+`Daily_Proportions.pdf`: timelines of numbers and cumulative duraitons of locations that fulfill the required property by individual and averages as last plot.
+
+### Parameters 
+`variab`: Name of the required data attribute. Take care that this parameter also exists in the Track Attributes of the input data set.
+
+`rel`: By this parameter the relation in the required property has to be selected. The possible values differ by parameter data type, only numeric and timestamps variables can relate by '==', '>' or '<'.
+
+`valu`: Value of the relation that the data set has to fullfill. In case of `rel` = 'is one of the following' commas have to be used to separate the possible values. In case of a timestamp parameter please use the timestamp format with year, month, day, hour, minute and second as in the example: '2021-06-23 09:34:00"
+
+`time`: Please tick this parameter if your selected variable is a timestamp type, so that the App can properly work with it.
+
+### Null or error handling:
+**Parameter `variab`:** If there is no individual variable with the name given here, an error will be returned.
+
+**Parameter `rel`:** If none of the relation options are selected, an error will be returned. It has to be carefully considered that the selected relation fits with the data type of the selected variable. Only numeric and timestamps variables can relate by '==', '>' or '<'.
+
+**Parameter `valu`:** If there is no value entered, an error will be returned. The data type of the entered value has to fit with the selected variable.
+
+**Parameter `time`:** If the selected variable is a timestamp and it was not indicated here, the variable will be treated as a string of text and possibly not handled correctly, leading to errors. Similarly if your variable is not a timestamp and it is indicated here. Default is 'false'.
+
+**Data:** The full data set is returned.
---
+++ RFunction.R
@@ -0,0 +1,134 @@
+library('move')
+library('foreach')
+library('data.table')
+library('ggplot2')
+
+rFunction <- function(data,variab,rel,valu,time=FALSE)
+{
+  Sys.setenv(tz="UTC")
+  
+  if (is.null(variab) | is.null(rel) | is.null(valu)) logger.info("One of your parameters has not been set. This will lead to an error.")
+  
+  if (variab %in% names(data))
+  {
+
+    if (rel=="%in%") #works for numeric or character values
+    {
+      valus <- strsplit(as.character(valu),",")[[1]]
+      
+      if (any(data@data[[variab]] %in% valus))
+      {
+        data.split <- move::split(data)
+        
+        perc_sel_list <- foreach(datai = data.split) %do% {
+          idi <- namesIndiv(datai)
+          logger.info(idi)
+          
+          datum <- as.Date(timestamps(datai))
+          datumi <- unique(datum)
+          perc_pts <- perc_dur <- numeric(length(datumi))
+          
+          for (i in seq(along=as.character(datumi)))
+          {
+            ix_d <- which(datum==datumi[i])
+            dataid <- datai[ix_d]
+            if (length(datai)>max(ix_d)) dataidp <- datai[unique(c(ix_d,ix_d+1))] else dataidp <- dataid
+            
+            perc_pts[i] <- length(which(dataid@data[[variab]] %in% valus))/length(dataid)
+            
+            dur <- timeLag(dataidp,units="hours") 
+            if (length(datai)<=max(ix_d)) dur <- c(dur,NA)
+            perc_dur[i] <- sum(dur[which(dataid@data[[variab]] %in% valus)],na.rm=TRUE)/24 #not correct if flight detection in data collection
+          }
+          perc_seli <- data.frame("trackId"=rep(idi[1],length(datumi)),"date"=datumi,"n.pts"=as.numeric(table(datum)),perc_pts,perc_dur)
+        }
+        perc_sel <- data.frame(rbindlist(perc_sel_list))
+
+      } else logger.info("None of your data fulfill the required property. Go back and reconfigure the App.")
+      
+    } else
+    {
+      if (time==TRUE) fullrel <- eval(parse(text=paste0("as.POSIXct(data@data$",variab,") ",rel," as.POSIXct('",valu,"')"))) else fullrel <- eval(parse(text=paste0("as.POSIXct(data@data$",variab,") ",rel," as.POSIXct('",valu,"')")))
+
+      if (any(fullrel)) #only do calculations if any locations fulfill the relation
+      {
+        data.split <- move::split(data)
+        
+        perc_sel_list <- foreach(datai = data.split) %do% {
+          idi <- namesIndiv(datai)
+          logger.info(idi)
+          
+          datum <- as.Date(timestamps(datai))
+          datumi <- unique(datum)
+          perc_pts <- perc_dur <- numeric(length(datumi))
+          
+          for (i in seq(along=as.character(datumi)))
+          {
+            ix_d <- which(datum==datumi[i])
+            dataid <- datai[ix_d]
+            if (length(datai)>max(ix_d)) dataidp <- datai[unique(c(ix_d,ix_d+1))] else dataidp <- dataid
+            
+            perc_pts[i] <- eval(parse(text=paste0("length(which(dataid@data$",variab,rel,valu,"))/length(dataid)")))
+            
+            dur <- timeLag(dataidp,units="hours") 
+            if (length(datai)<=max(ix_d)) dur <- c(dur,NA)
+            perc_dur[i] <- eval(parse(text=paste0("sum(dur[which(dataid@data$",variab,rel,valu,")],na.rm=TRUE)/24"))) #not correct if flight detection in data collection
+          }
+          perc_seli <- data.frame("trackId"=rep(idi[1],length(datumi)),"date"=datumi,"n.pts"=as.numeric(table(datum)),perc_pts,perc_dur)
+        }
+        perc_sel <- data.frame(rbindlist(perc_sel_list))
+        
+      } else logger.info("None of your data fulfill the required property. Go back and reconfigure the App.")
+
+    }
+  } else logger.info("You selected variable is not available in the data set. Go back and reconfigure the App.")
+
+  
+  if (exists("perc_sel")) #if calculations have been made
+  {
+    dates <- unique(perc_sel$date)
+    n.ids <- as.numeric(table(perc_sel$date))
+    perc_pts_avg <- apply(matrix(dates),1,function(x) mean(perc_sel$perc_pts[perc_sel$date==x]))
+    perc_dur_avg <- apply(matrix(dates),1,function(x) mean(perc_sel$perc_dur[perc_sel$date==x]))
+    
+    perc_sel_avg <- data.frame("trackId"="avg","date"=dates,"n.pts"=n.ids,"perc_pts"=perc_pts_avg,"perc_dur"=perc_dur_avg) 
+    perc_sel_list <- c(perc_sel_list,list(perc_sel_avg))
+    perc_sel <- rbind(perc_sel,perc_sel_avg)
+    write.csv(perc_sel,file=paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "Daily_Proportions.csv"),row.names=FALSE)
+    
+    pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "Daily_Proportions.pdf"),width=12,height=8)
+    lapply(perc_sel_list, function(z){
+      plot_percpts <- ggplot(z,aes(date,perc_pts))+
+        geom_line(color="red")+
+        geom_point()+
+        facet_grid(~trackId)+
+        theme_bw()+
+        labs(x="", y="Daily Proportion - Points")
+      print(plot_percpts)
+    })
+    lapply(perc_sel_list, function(z){
+      plot_percdur <- ggplot(z,aes(date,perc_dur))+
+        geom_line(color="blue")+
+        geom_point()+
+        facet_grid(~trackId)+
+        theme_bw()+
+        labs(x="", y="Daily Proportion - Duration")
+      print(plot_percdur)
+    })
+    dev.off()
+  }
+  
+  result <- data #return full data set
+  return(result)
+}
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
